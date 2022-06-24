9fc9676e49752cb8268f002454bcc8e0ad62cc4f
Esemwy <github.5.smy@neverbox.com>
2022-06-24 12:23:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-compendium [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Generate Foundry Compendium Module
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-compendium using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-compendium
+```
+
+Then generate your new project:
+
+```bash
+yo compendium
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Esemwy](https://github.com/esemwy/)
+
+
+[npm-image]: https://badge.fury.io/js/generator-compendium.svg
+[npm-url]: https://npmjs.org/package/generator-compendium
+[travis-image]: https://travis-ci.com/esemwy/generator-compendium.svg?branch=master
+[travis-url]: https://travis-ci.com/esemwy/generator-compendium
+[daviddm-image]: https://david-dm.org/esemwy/generator-compendium.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/esemwy/generator-compendium
---
+++ generators/app/index.js
@@ -0,0 +1,205 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const fs = require('fs');
+
+module.exports = class extends Generator {
+
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the tiptop ${chalk.red('generator-compendium')} generator!`
+      )
+    );
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter a module name, lower case only, with dashes as separators:',
+        validate: function (input) {
+            // Declare function as asynchronous, and save the done callback
+            var done = this.async();
+        
+            // Do async stuff
+            setTimeout(function() {
+              if (input.match(/[^a-z0-5\-]/) !== null) {
+                // Pass the return value in the done callback
+                done('Only lower case characters and dashes are allowed');
+                return;
+              }
+              // Pass the return value in the done callback
+              done(null, true);
+            }, 500);
+        }
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your compendium:'
+      },
+      {
+        type: 'input',
+        name: 'abbreviation',
+        message: 'Enter an abbreviation for your compendium:'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your compendium:'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Enter author name of your compendium:'
+      },
+      {
+        type: "input",
+        name: "url",
+        message: 'Enter project URL:',
+        validate: function (string) {
+            var done = this.async();
+          
+            // Do async stuff
+            setTimeout(function() {
+              let url;
+              
+              try {
+                url = new URL(string);
+              } catch (_) {
+                done("Must be a valid URL.")
+                return;  
+              }
+          
+              if (url.protocol === "http:" || url.protocol === "https:") {
+                done(null, true);
+              }
+              else {
+                done('Protocol must be "http" or "https"')
+              }
+          }, 500);
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'actorTypes',
+        message: 'Which Actor compendiums should be created?',
+        choices: [
+          {
+            value: 'Player Characters',
+            name: 'Player Characters',
+            checked: false
+          },
+          {
+            value: 'NPCs',
+            name: 'NPCs',
+            checked: false
+          },
+          {
+            value: 'Monsters',
+            name: 'Monsters',
+            checked: true
+          }
+        ]
+      },
+      {
+        type: 'checkbox',
+        name: 'itemTypes',
+        message: 'Which Item compendiums should be created?',
+        choices: [
+          {
+            value: 'Feats',
+            name: 'Feats',
+            checked: false
+          },
+          {
+            value: 'Classes',
+            name: 'Classes',
+            checked: true
+          },
+          {
+            value: 'Class Features',
+            name: 'Classe Features',
+            checked: true
+          },
+          {
+            value: 'Races',
+            name: 'Races',
+            checked: true
+          },
+          {
+            value: 'Racial Features',
+            name: 'Racial Features',
+            checked: true
+          },
+          {
+            value: 'Epuipment',
+            name: 'Equipment',
+            checked: true
+          },
+          {
+            value: 'Spells',
+            name: 'Spells',
+            checked: true
+          }
+        ]
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+      console.log(props);
+    });
+  }
+
+  writing() {
+    let module_json = {
+      "version": "1.0.0",
+      "minimumCoreVersion": "9",
+      "compatibleCoreVersion": "9",
+      "author": this.props.author,
+      "name": this.props.name,
+      "title": this.props.title,
+      "description": this.props.description,
+      "author": this.props.author,
+      "url": this.props.url,
+      "packs": []
+    };
+
+    this.props.actorTypes.forEach(element => {
+      const name = element.toLowerCase().replace(' ','')
+      module_json.packs.push({
+        "name": name,
+        "label": `${element} (${this.props.abbreviation})`,
+        "path": `./packs/${name}.db`,
+        "module": this.props.name,
+        "entity": "Actor"
+      });
+    });
+    this.props.itemTypes.forEach(element => {
+      const name = element.toLowerCase().replace(' ','')
+      module_json.packs.push({
+        "name": name,
+        "label": `${element} (${this.props.abbreviation})`,
+        "path": `./packs/${name}.db`,
+        "module": this.props.name,
+        "entity": "Item"
+      });
+    });
+    this.module_json = module_json
+  }
+
+
+  install() {
+    fs.mkdirSync(this.props.name);
+    fs.mkdirSync(`${this.props.name}/packs`);
+    this.module_json.packs.forEach(pack => {
+      fs.writeFileSync(`${this.props.name}/${pack.path}`,'');
+    });
+    fs.writeFileSync(`${this.props.name}/module.json`, JSON.stringify(this.module_json, undefined, 2));
+    console.log(this.module_json);
+  }
+};
