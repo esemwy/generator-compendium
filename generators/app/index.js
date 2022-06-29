fa688d8392b9a219718b40ac55b40907cbb669cf
Esemwy <github.5.smy@neverbox.com>
2022-06-29 14:47:44 -0700

    lint fixes

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,147 +1,147 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const fs = require('fs');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const fs = require("fs");
 
 module.exports = class extends Generator {
-
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the tiptop ${chalk.red('generator-compendium')} generator!`
+        `Welcome to the tiptop ${chalk.red("generator-compendium")} generator!`
       )
     );
 
     const prompts = [
       {
-        type: 'input',
-        name: 'name',
-        message: 'Enter a module name, lower case only, with dashes as separators:',
-        validate: function (input) {
-            // Declare function as asynchronous, and save the done callback
-            var done = this.async();
-        
-            // Do async stuff
-            setTimeout(function() {
-              if (input.match(/[^a-z0-5\-]/) !== null) {
-                // Pass the return value in the done callback
-                done('Only lower case characters and dashes are allowed');
-                return;
-              }
-              // Pass the return value in the done callback
-              done(null, true);
-            }, 500);
-        }
-      },
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of your compendium:'
-      },
-      {
-        type: 'input',
-        name: 'abbreviation',
-        message: 'Enter an abbreviation for your compendium:'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description of your compendium:'
-      },
-      {
-        type: 'input',
-        name: 'author',
-        message: 'Enter author name of your compendium:'
-      },
-      {
         type: "input",
-        name: "url",
-        message: 'Enter project URL:',
-        validate: function (string) {
-            var done = this.async();
-          
-            // Do async stuff
-            setTimeout(function() {
-              let url;
-              
-              try {
-                url = new URL(string);
-              } catch (_) {
-                done("Must be a valid URL.")
-                return;  
-              }
-          
-              if (url.protocol === "http:" || url.protocol === "https:") {
-                done(null, true);
-              }
-              else {
-                done('Protocol must be "http" or "https"')
-              }
+        name: "name",
+        message:
+          "Enter a module name, lower case only, with dashes as separators:",
+        validate: function(input) {
+          // Declare function as asynchronous, and save the done callback
+          var done = this.async();
+
+          // Do async stuff
+          setTimeout(function() {
+            if (input.match(/[^a-z0-5-]/) !== null) {
+              // Pass the return value in the done callback
+              done("Only lower case characters and dashes are allowed");
+              return;
+            }
+
+            // Pass the return value in the done callback
+            done(null, true);
           }, 500);
         }
       },
       {
-        type: 'checkbox',
-        name: 'actorTypes',
-        message: 'Which Actor compendiums should be created?',
+        type: "input",
+        name: "title",
+        message: "Enter the title of your compendium:"
+      },
+      {
+        type: "input",
+        name: "abbreviation",
+        message: "Enter an abbreviation for your compendium:"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your compendium:"
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "Enter author name of your compendium:"
+      },
+      {
+        type: "input",
+        name: "url",
+        message: "Enter project URL:",
+        validate: function(string) {
+          var done = this.async();
+
+          // Do async stuff
+          setTimeout(function() {
+            let url;
+
+            try {
+              url = new URL(string);
+            } catch (_) {
+              done("Must be a valid URL.");
+              return;
+            }
+
+            if (url.protocol === "http:" || url.protocol === "https:") {
+              done(null, true);
+            } else {
+              done('Protocol must be "http" or "https"');
+            }
+          }, 500);
+        }
+      },
+      {
+        type: "checkbox",
+        name: "actorTypes",
+        message: "Which Actor compendiums should be created?",
         choices: [
           {
-            value: 'Player Characters',
-            name: 'Player Characters',
+            value: "Player Characters",
+            name: "Player Characters",
             checked: false
           },
           {
-            value: 'NPCs',
-            name: 'NPCs',
+            value: "NPCs",
+            name: "NPCs",
             checked: false
           },
           {
-            value: 'Monsters',
-            name: 'Monsters',
+            value: "Monsters",
+            name: "Monsters",
             checked: true
           }
         ]
       },
       {
-        type: 'checkbox',
-        name: 'itemTypes',
-        message: 'Which Item compendiums should be created?',
+        type: "checkbox",
+        name: "itemTypes",
+        message: "Which Item compendiums should be created?",
         choices: [
           {
-            value: 'Feats',
-            name: 'Feats',
+            value: "Feats",
+            name: "Feats",
             checked: false
           },
           {
-            value: 'Classes',
-            name: 'Classes',
+            value: "Classes",
+            name: "Classes",
             checked: true
           },
           {
-            value: 'Class Features',
-            name: 'Classe Features',
+            value: "Class Features",
+            name: "Class Features",
             checked: true
           },
           {
-            value: 'Races',
-            name: 'Races',
+            value: "Races",
+            name: "Races",
             checked: true
           },
           {
-            value: 'Racial Features',
-            name: 'Racial Features',
+            value: "Racial Features",
+            name: "Racial Features",
             checked: true
           },
           {
-            value: 'Epuipment',
-            name: 'Equipment',
+            value: "Equipment",
+            name: "Equipment",
             checked: true
           },
           {
-            value: 'Spells',
-            name: 'Spells',
+            value: "Spells",
+            name: "Spells",
             checked: true
           }
         ]
@@ -156,50 +156,51 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    let module_json = {
-      "version": "1.0.0",
-      "minimumCoreVersion": "9",
-      "compatibleCoreVersion": "9",
-      "author": this.props.author,
-      "name": this.props.name,
-      "title": this.props.title,
-      "description": this.props.description,
-      "author": this.props.author,
-      "url": this.props.url,
-      "packs": []
+    let moduleJSON = {
+      version: "1.0.0",
+      minimumCoreVersion: "9",
+      compatibleCoreVersion: "9",
+      name: this.props.name,
+      title: this.props.title,
+      description: this.props.description,
+      author: this.props.author,
+      url: this.props.url,
+      packs: []
     };
 
     this.props.actorTypes.forEach(element => {
-      const name = element.toLowerCase().replace(' ','')
-      module_json.packs.push({
-        "name": name,
-        "label": `${element} (${this.props.abbreviation})`,
-        "path": `./packs/${name}.db`,
-        "module": this.props.name,
-        "entity": "Actor"
+      const name = element.toLowerCase().replace(" ", "");
+      moduleJSON.packs.push({
+        name: name,
+        label: `${element} (${this.props.abbreviation})`,
+        path: `./packs/${name}.db`,
+        module: this.props.name,
+        entity: "Actor"
       });
     });
     this.props.itemTypes.forEach(element => {
-      const name = element.toLowerCase().replace(' ','')
-      module_json.packs.push({
-        "name": name,
-        "label": `${element} (${this.props.abbreviation})`,
-        "path": `./packs/${name}.db`,
-        "module": this.props.name,
-        "entity": "Item"
+      const name = element.toLowerCase().replace(" ", "");
+      moduleJSON.packs.push({
+        name: name,
+        label: `${element} (${this.props.abbreviation})`,
+        path: `./packs/${name}.db`,
+        module: this.props.name,
+        entity: "Item"
       });
     });
-    this.module_json = module_json
+    this.moduleJSON = moduleJSON;
   }
-
 
   install() {
     fs.mkdirSync(this.props.name);
     fs.mkdirSync(`${this.props.name}/packs`);
-    this.module_json.packs.forEach(pack => {
-      fs.writeFileSync(`${this.props.name}/${pack.path}`,'');
+    this.moduleJSON.packs.forEach(pack => {
+      fs.writeFileSync(`${this.props.name}/${pack.path}`, "");
     });
-    fs.writeFileSync(`${this.props.name}/module.json`, JSON.stringify(this.module_json, undefined, 2));
-    console.log(this.module_json);
+    fs.writeFileSync(
+      `${this.props.name}/module.json`,
+      JSON.stringify(this.moduleJSON, undefined, 2)
+    );
+    console.log(this.moduleJSON);
   }
 };
